d759908520689e215ccd758872b88a7c4ea0ffdc
Naty28147 <naty28147@gmail.com>
2023-11-24 16:28:02 -0300

    ...

--- index.js
+++ index.js
@@ -1,41 +1,78 @@
 const express = require('express')
 const app = express()
-const exphbs = require('express-handlebars')
 const port = 3000
+const exphbs = require('express-handlebars')
 
+
+
+
+//configure template handlebars
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+
+//parser para leitura do body
 app.use(
-  express.urlencoded({
-    extended: true
-  })
+express.urlencoded({
+extended: true
+})
 )
 app.use(express.json())
 
+//ADICIONANDO CAMINHO CSS
 app.use(express.static('public'))
 
 app.get('/users/add', (req, res) => {
-  res.render('userform')
-  })
-  
+res.render('userform')
+})
+
 app.post('/users/save', (req, res) => {
-  const nome_produto = req.body.nome_produto;
-  const nome_fornecedor = req.body.nome_fornecedor;
-  const data_compra = req.body.data_compra;
-  const valor_compra = req.body.valor_compra;
-  const valor_venda = req.body.valor_venda;
-  const categoria = req.body.categoria;
-  
-  const user = { nome_produto: nome_produto, nome_fornecedor: nome_fornecedor, data_compra: data_compra, valor_compra: valor_compra, valor_venda: valor_venda, categoria: categoria }
-  res.render('viewuser', { user: user })
-  
-  })
+const name = req.body.name
+const age = req.body.age
+const user = { name: name, age: age }
+res.render('viewuser', { user: user })
+
+})
+
+const usuario = {
+login: 'teste',
+senha: 123
+
+}
+
 
 app.get('/', (req, res) => {
-    res.render('home')
-  })  
 
+res.render('login')
+})
+
+var auth = false
+
+app.post('/user/login', (req, res) => {
+const login = req.body.login
+const senha = req.body.senha
+let message = ""
+
+if (login == usuario.login && senha == usuario.senha) {
+auth = true
+message = "Usuário logado com sucesso!"
+res.render('home', { usuario: usuario, auth, message })
+}
+else {
+auth = false
+message = "Usuário e/ou senha inválidos!"
+res.render('login', { auth, message })
+}
+
+})
+
+//pagina 404
+app.use(function (req, res, next) {
+res.status(404).render('404')
+})
+
+
+//webserver
 app.listen(port, () => {
-  console.log('Server online!')
+console.log('Server Started')
 })
